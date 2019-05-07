0ecb1f7a587aefa9056ffc147cc13011c9fc948f
Choudhary, Yogesh <Yogesh.Choudhary@capita.co.uk>
2019-05-07 05:55:12 +0530

    Included custom search box

--- www/js/indexCtrl.js
+++ www/js/indexCtrl.js
@@ -1,28 +1,46 @@
-(function () {
-    'use strict';
+(function() {
+  "use strict";
 
-    var indexCtrl = function ($scope, $ionicPopup) {
-        $scope.onClick = function () {
-            var okPopup = $ionicPopup.alert({
-                title: 'Firebase Analytics',
-                template: 'Log a custom event in Firebase Analytics'
-            });
-
-            okPopup.then(function (res) {
-                console.log("Do the stuff here");
-                if (window.cordova 
-                    && window.cordova.plugins
-                    && window.cordova.plugins.firebase 
-                    && window.cordova.plugins.firebase.analytics) {
-                        window.cordova.plugins.firebase.analytics.logEvent("custom_event", { currView: 'search' });
-                } else {
-                    console.error("UNDEFINED!! window.cordova.plugins.firebase.analytics");
-                }
-            });
-        };
+  var indexCtrl = function($scope, $ionicPopup) {
+    var isCordova = function() {
+      return (
+        window.cordova &&
+        window.cordova.plugins &&
+        window.cordova.plugins.firebase &&
+        window.cordova.plugins.firebase.analytics
+      );
     };
 
-    angular
-        .module('starter')
-        .controller('indexCtrl', indexCtrl);
+    $scope.onClick = function() {
+      var searchBox = $ionicPopup.show({
+        title: "Firebase Analytics",
+        subTitle: "Enter the text to search on",
+        template: '<input type="text" ng-model="data.searchText"></input>',
+        scope: $scope,
+        buttons: [
+          { text: "Cancel" },
+          {
+            text: "Search",
+            type: "button-positive"
+          }
+        ]
+      });
+
+      searchBox.then(function(res) {
+        console.log("Do the stuff here");
+        if (isCordova()) {
+          window.cordova.plugins.firebase.analytics.logEvent("custom_event", {
+            currView: "search",
+            searchOn: res
+          });
+        } else {
+          console.error(
+            "UNDEFINED!! window.cordova.plugins.firebase.analytics"
+          );
+        }
+      });
+    };
+  };
+
+  angular.module("starter").controller("indexCtrl", indexCtrl);
 })();
